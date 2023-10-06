93af725589002c8533830af5b313399751decca8
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-06 23:02:44 +0100

    Initial commiy

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Typed module sum_mixed_list"""
+
+
+def sum_mixed_list(mxd_list: list[int, float]) -> float:
+    """Returns sum of floats and ints in list"""
+    return float(sum(mxd_list))
