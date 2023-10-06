4b3770dd28fc3d7b411b301860214ded0f956acb
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-06 23:21:04 +0100

    For old version

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Typed module sum_mixed_list"""
+from typing import Union
 
-
-def sum_mixed_list(mxd_list: list[int, float]) -> float:
+def sum_mixed_list(mxd_list: list[Union[int, float]]) -> float:
     """Returns sum of floats and ints in list"""
     return float(sum(mxd_list))
