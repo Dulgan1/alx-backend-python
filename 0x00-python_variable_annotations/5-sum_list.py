5a60a7a003c96b53382066708742e08a56f8bc3c
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-06 22:32:20 +0100

    Doc

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,7 +3,8 @@
 
 
 def sum_list(input_list: list[float]) -> float:
-    sum = 0
+    """Returns sum of floats un input list"""
+    s = 0
     for flt in input_list:
-        sum += flt
-    return float(sum)
+        s += flt
+    return float(s)
