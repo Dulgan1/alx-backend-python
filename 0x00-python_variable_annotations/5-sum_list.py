1f67980b8ed544040cffded7b8923909c0c0b3e2
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-07 18:21:09 +0100

    old version

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Typed module sum_list"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Returns sum of floats un input list"""
     s: float = 0.0
     for flt in input_list:
