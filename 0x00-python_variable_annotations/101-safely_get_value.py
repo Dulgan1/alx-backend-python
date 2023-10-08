bfcbb016ed6e46fd7264fe211893290abf70b17c
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-08 22:54:43 +0100

    Extend mapping

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,7 +4,7 @@ from typing import TypeVar, Mapping, Any, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any], key: Any,
+def safely_get_value(dct: Mapping[Any, Union[Any, T]], key: Any,
                      default: Union[T, None]) -> Union[Any, T]:
     """Returns value of key in map-type object if exists"""
     if key in dct:
