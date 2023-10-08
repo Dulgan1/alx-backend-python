7df5422dbc0fb021c00879600df2278f416e4ba6
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-08 23:15:24 +0100

    Doc

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,6 +4,8 @@ from typing import List, Union
 
 
 def zoom_array(lst: List, factor: Union[int, float] = 2) -> List:
+    """Returns zoomed list of passed list by factor of factor passed
+    argument"""
     zoomed_in: List = [
         item for item in lst
         for i in range(int(factor))
