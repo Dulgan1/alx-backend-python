f01122a54bff9984a5dd58f3b0289d45247700ae
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-08 22:28:59 +0100

    TypeVar and other imports

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Type annotated module"""
-from typing import TypeVar, Mapping, Any
+from typing import TypeVar, Mapping, Any, Union
+T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping[Any], key: Any,
-                     default: Union[Any, None]) -> Any:
+                     default: Union[T, None]) -> Union[Any, T]:
     """Returns value of key in map-type object if exists"""
     if key in dct:
         return dct[key]
