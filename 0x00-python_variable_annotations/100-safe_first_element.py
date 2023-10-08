9382fd409bae5b60cf6bc90e8a86cc0f75cf75cf
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-08 16:21:17 +0100

    Initial commiy

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Tyoe annotated module"""
+from typing import Sequence, Any, Unioin
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Return any or None from passed sequence"""
+    if lst:
+        return lst[0]
+    else:
+        return None
