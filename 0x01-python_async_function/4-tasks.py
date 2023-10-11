6a1fafc5004e8de89964463060099fd83333a36c
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-11 23:05:08 +0100

    Typo fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -14,5 +14,5 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     coroutine_tasks: list = [task_wait_random(max_delay)
                              for _ in range(n)]
     wait_list = [await task for task in  asyncio.\
-            as_completed(coroutin_tasks)]
+            as_completed(coroutine_tasks)]
     return wait_list
