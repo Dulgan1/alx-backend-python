09ee13c007a1da9e3796df94ae760f9549005339
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-11 22:55:12 +0100

    right annotation

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,7 +10,7 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Task Based alt of wait_n from 1-concurrent_coroutines.py:
         Returns list of results of an async coroutine spawn n times
     """
-    wait_list: list
+    wait_list: List[float]
     coroutine_tasks: list = [task_wait_random(max_delay)
                              for _ in range(n)]
     wait_list = [await task for task in  asyncio.\
