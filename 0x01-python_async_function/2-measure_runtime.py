22e4ef52e7d04740fa80d06d86e58255ac1b44a0
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-11 22:06:39 +0100

    Using performance count instead of current time

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 async def measure_time(n: int, max_delay: int) -> float:
     """Returns approx time for running async wait_n"""
-    start = time.time()
+    start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end = time.time()
+    end = time.perf_counter()
     return (end - start)
