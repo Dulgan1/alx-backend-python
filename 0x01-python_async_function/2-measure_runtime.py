9b2893d6844ac7c9f4a257be8610c028d3a1c0a9
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-11 23:43:27 +0100

    not an async func

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Async module"""
+"""non-Async module"""
 import asyncio
 import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Returns approx time for running async wait_n"""
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
