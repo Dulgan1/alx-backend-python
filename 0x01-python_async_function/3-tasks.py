9a67a2b9ade5840bc2bd5c87cdd55bbcf54303ca
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-11 22:16:01 +0100

    Pycodestyle

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,6 +5,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """Creates and return an asyncio task of async coroutine: 
+    """Creates and return an asyncio task of async coroutine:
     wait_random accepting max_delat as arg"""
     return asyncio.create_task(wait_random(max_delay))
