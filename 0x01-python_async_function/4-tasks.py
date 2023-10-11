a5c968bd31799879ed470d4fab348b63fea36b54
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-11 22:53:19 +0100

    Initial commit

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Async coroutine"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-task').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Task Based alt of wait_n from 1-concurrent_coroutines.py:
+        Returns list of results of an async coroutine spawn n times
+    """
+    wait_list: list
+    coroutine_tasks: list = [task_wait_random(max_delay)
+                             for _ in range(n)]
+    wait_list = [await task for task in  asyncio.\
+            as_completed(coroutin_tasks)]
+    return wait_list
