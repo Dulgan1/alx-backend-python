15ff92c2b50d5a39cecc423223dc07e7e9d0b480
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-11 21:10:16 +0100

    Documentation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns list of results of an async coroutine spawn n times"""
     wait_list: list
     coroutines: list = [wait_random(max_delay) for i in range(n)]
     returns: list[float] = await asyncio.gather(*coroutines)
