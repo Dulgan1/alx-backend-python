b4e14b80d831693be77f549cf948d5d70d21ec02
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-13 15:35:33 +0100

    Doc

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+"""Async module, generates list of floats"""
 from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """Using asynchronous comprehension"""
     return [rf async for rf in async_generator()]
